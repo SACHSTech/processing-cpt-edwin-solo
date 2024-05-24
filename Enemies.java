eae2f2b32a0a4f8bd7e7a85ca7ebe8e30ae73913
Edwin_liu1 <Edwin.liu25@ycdsbk12.ca>
2024-05-24 14:26:51 -0400

    Day 1 (nothing happened)

---
+++ Enemies.java
@@ -0,0 +1,17 @@
+import processing.core.PApplet;
+
+public class Enemies {
+    int x, y;
+    Enemies(int Xposition, int Yposition){
+        x = Xposition;
+        y = Yposition;
+    }
+    class Skeleton(){
+        Skeleton(){
+            
+        }
+    }
+
+
+    
+}
